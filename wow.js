a44f96c63ef55b666f2970de9bf9d3b79462134c
boooonN <rnqhsgur1128@naver.com>
2024-08-23 21:44:45 +0900

    slide
    slide

--- wow.js
+++ wow.js
@@ -1,3 +1,16 @@
-var Swiper = new Swiper(".container", {
-  navigation: { nextE1: ".btn-pre", prevE1: "btn-next" },
+var Swiper = new Swiper(".swiper-container", {
+  slidesPreView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    /// disableOnInteraction은 기본값은 true로 사용자가 상호작용 (예를 들어 마우스로 슬라이드를 수동 전환하는 등) 하게 될 경우 자동 재생이 멈추게 된다.///
+  },
+  loop: false,
+  loopAdditionalSlides: 1,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
